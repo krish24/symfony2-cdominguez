c8151c09c96614b8ced8158b0ee863d927a23185
krlosnando@gmail.com <krlosnando@gmail.com@aa412c79-fcdb-bf72-8fbc-3546aae4f2a0>
2012-05-21 14:36:15 +0000

    Administrator commit

---
+++ src/Base/EJSTreeGridBundle/BaseEJSTreeGridBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Base\EJSTreeGridBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BaseEJSTreeGridBundle extends Bundle
+{
+}
---
+++ src/Base/JQueryUIBundle/BaseJQueryUIBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Base\JQueryUIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BaseJQueryUIBundle extends Bundle
+{
+}
