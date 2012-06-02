62c8220b95cbdd7ee0a04af55c1a9ab685d72b38
krlosnando@gmail.com <krlosnando@gmail.com@aa412c79-fcdb-bf72-8fbc-3546aae4f2a0>
2012-06-02 23:34:13 +0000

    Avances

--- trunk/src/MySite/CDominguezBundle/Resources/public/js/index.js
+++ trunk/src/MySite/CDominguezBundle/Resources/public/js/index.js
@@ -4,7 +4,7 @@
         a(".da-circular-stat").daCircularStat();
         
         //Crea el Spinner
-        a("#spinner-gasto").spinner({
+        a("#_cantidad").spinner({
             prefix  : "¢ ",
             min     : 0,
             max     : 100000,
@@ -12,20 +12,96 @@
             step    : 1
         });
         
-        //Crea el chosen
-        a("select").chosen();
+        //Crea el chosen para el index
+        a("#form-add-gasto select").chosen();
         
-        a("select").change(function(){
-            var e = a(this).find(':selected');
-            var idE = "#add-" + a(this).attr('id');
-            console.log(idE);
-            if(a(e).val() == 1){
-                a(idE).fadeIn('slow');
-            }else{
-                if(a(idE).css('display') != 'none'){
-                    a(idE).effect( 'explode', {}, 1000 );
-                }
-            }
-        });
-    })
+        a("#form-add-gasto select").change(function(){
+            addInputChosen(this);
+        })
+    });
 })(jQuery);
+
+var flagValidate = 0;
+    
+function validateForm(sendForm){
+    var enviarForm = sendForm;
+    
+    var chosens = $("#form-add-gasto select");
+    //Validar los Chosen
+    $.each(chosens, function(){
+        var id = $(this).attr('id');
+        var c = $('#' + id + ' :selected');
+        var t = $('#add' + id);
+        var l = $('#label' + id);
+
+        if(c.val() == -1){
+            if(t.val() == ''){
+                l.css('display', '');
+                t.addClass('error');
+                t.fadeIn('slow');
+                enviarForm = false;
+            }else{
+                l.css('display', 'none');
+                t.removeClass('error');
+            }
+        }else{
+            l.css('display', 'none');
+            t.removeClass('error');
+        }
+    });
+    
+    //Validamos si ingreso la cantidad.
+    var d = $('#_cantidad');
+    var t = $('#label_cantidad');
+    if(d.val() == '¢ 0' || d.val() == '0' || d.val() == ''){
+        t.css('display', '');
+        d.addClass('error');
+        enviarForm = false;
+    }else{
+        t.css('display', 'none');
+        d.removeClass('error');
+    }
+    
+    //Eliminar los elementos cuando cambie algo, agregar el evento solo 1 vez con el flag.
+    if(flagValidate == 0){
+        $("#form-add-gasto input, select").bind("change",function(){
+            validateForm(false);
+        });
+        flagValidate = 1;
+    }
+    
+    if(enviarForm){
+        saveData();
+    }
+} 
+
+function addInputChosen(element){
+    var id = $(element).attr('id');
+    var e = $('#' + id + ' :selected');
+    var idE = "#add" + id;
+    if($(e).val() == -1){
+        $(idE).fadeIn('slow');
+    }else{
+        if($(idE).css('display') != 'none'){
+            $(idE).effect( 'explode', {}, 1000 );
+            $('#label' + id).css('display', 'none');
+        }
+    }
+}
+
+function saveData(){
+    $.ajax({
+        url: _cdominguez.urls.add,
+        data: $("#form-add-gasto").serialize(),
+        success: function(r){
+            if(r == 'done'){
+                showMessage('success', 'Los datos se guardaron correctamente');
+            }
+        },
+        beforeSend: function(){},
+        error:function (xhr, ajaxOptions, thrownError){
+            showMessage('error', 'An error occurred, please try again.' + thrownError);
+        },
+        type: "POST"
+    });
+}
