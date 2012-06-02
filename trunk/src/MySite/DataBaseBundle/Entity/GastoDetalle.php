88a0daa3b0bbe6fd591a662b1b6e4211d6b0a663
krlosnando@gmail.com <krlosnando@gmail.com@aa412c79-fcdb-bf72-8fbc-3546aae4f2a0>
2012-06-02 23:33:07 +0000

    Avances

---
+++ trunk/src/MySite/DataBaseBundle/Entity/GastoDetalle.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace MySite\DataBaseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MySite\DataBaseBundle\Entity\GastoDetalle
+ *
+ * @ORM\Table(name="GastoDetalle")
+ * @ORM\Entity(repositoryClass="MySite\DataBaseBundle\Repository\GastoDetalleRepository")
+ */
+class GastoDetalle
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $descripcion
+     *
+     * @ORM\Column(name="descripcion", type="string", length=255)
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Categoria", inversedBy="gastoDetalles", cascade={"persist"})
+     * @ORM\JoinColumn(name="idcategoria", referencedColumnName="id")
+     */
+    private $categoria;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Gasto", mappedBy="detalle", cascade={"persist"})
+     */
+    private $gastos;
+    
+    
+    public function __construct()
+    {
+        $this->gastos = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string 
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set categoria
+     *
+     * @param MySite\DataBaseBundle\Entity\Categoria $categoria
+     */
+    public function setCategoria(\MySite\DataBaseBundle\Entity\Categoria $categoria)
+    {
+        $this->categoria = $categoria;
+    }
+
+    /**
+     * Get categoria
+     *
+     * @return MySite\DataBaseBundle\Entity\Categoria 
+     */
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+    
+    /**
+     * Add gastos
+     *
+     * @param MySite\DataBaseBundle\Entity\Gasto $gastos
+     */
+    public function addGasto(\MySite\DataBaseBundle\Entity\Gasto $gastos)
+    {
+        $this->gastos[] = $gastos;
+    }
+
+    /**
+     * Get gastos
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getGastos()
+    {
+        return $this->gastos;
+    }
+}
