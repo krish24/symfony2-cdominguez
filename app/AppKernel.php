b261d323ea5c52b1891f14dcae153e48cdc7ea39
krlosnando@gmail.com <krlosnando@gmail.com@aa412c79-fcdb-bf72-8fbc-3546aae4f2a0>
2012-06-26 02:38:24 +0000

    Avances

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,6 +32,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new \Sf2gen\Bundle\ConsoleBundle\Sf2genConsoleBundle();
         }
 
         return $bundles;
