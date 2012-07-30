f9bd5aee88b33454175301fe13d4397a3a0d6df6
krlosnando@gmail.com <krlosnando@gmail.com@aa412c79-fcdb-bf72-8fbc-3546aae4f2a0>
2012-07-30 02:26:56 +0000

    Avances

--- web/js/base.js
+++ web/js/base.js
@@ -2,27 +2,33 @@
 * Funcion que se encarga de mostrar los mensajes de error y de información.
 ********************************************************************************************************************************************/
 function showMessage(ptypeMsg, ptext){
-    $('#msg').html('');
     var msg = '';
     switch(ptypeMsg){
         case 'error':
             msg = '<div class="da-message error">' + ptext + '</div>';
-            $('#msg').append(msg);
+            $('#msg').html(msg);
             break;
         case 'success':
             msg = '<div class="da-message success">' + ptext + '</div>';
-            $('#msg').append(msg);
+            $('#msg').html(msg);
             break;
         case 'info':
             msg = '<div class="da-message info">' + ptext + '</div>';
-            $('#msg').append(msg);
+            $('#msg').html(msg);
             break;
         case 'warning':
             msg = '<div class="da-message warning">' + ptext + '</div>';
-            $('#msg').append(msg);
+            $('#msg').html(msg);
             break;
     }
     //Mostramos el mensaje.
-    $('#msg').hide();
-    $('#msg').fadeIn(2000);
+    $('#msg').stop(true,true).hide();
+    $('#msg').fadeIn(2000).delay(20000);
+    $('#msg').animate({
+        opacity:0
+    },function(){
+        $(this).slideUp("normal",function(){
+            $(this).css("opacity","")
+        })
+    });
 }
